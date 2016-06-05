b34cbd70aff8815f7669b544bc48146fcf99fc98
cohadar <cohadar@gmail.com>
2016-06-05 02:13:36 +0200

    first

---
+++ Io/Io.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+/* Mighty Cohadar */
+public class Io {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		System.out.println(a + b);
+	}
+
+}
+
